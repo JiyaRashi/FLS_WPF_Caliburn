0a580f5a0f6b07a8420f80dbdf9b21d54df131ba
Jiyavudeen r <jiyavudeen.r379@outlook.com>
2020-09-19 21:36:51 +0530

    Home view model added

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLS_WPF_Caliburn.ViewModels
+{
+    public class HomeViewModel: Conductor<IScreen>.Collection.OneActive
+    {
+
+
+    }
+}
--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -1,13 +1,31 @@
 ﻿using Caliburn.Micro;
+using FLS_WPF_Caliburn.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FLS_WPF_Caliburn.ViewModels
 {
-    public class ShellViewModel :Screen
+    public class ShellViewModel : Conductor<IScreen>.Collection.OneActive
     {
+        private PlayersDetailsView playersDetailsView;
+
+        private  HomeViewModel homeArea;
+
+        public HomeViewModel HomeArea
+        {
+            get { return homeArea; }
+            set { homeArea = value; NotifyOfPropertyChange(() => HomeArea); }
+        }
+
+        public ShellViewModel(HomeViewModel homeViewModel)
+        {
+            this.HomeArea = new HomeViewModel();
+            ActivateItem(HomeArea);
+        }
+
     }
 }
